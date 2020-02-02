3585d05c03c5f1641a25400d2b436f606212261b
Maximilian Merz <git@mxmerz.de>
2020-02-02 16:18:09 +0100

    Update install script, gitignore

--- install.sh
+++ install.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 
-echo "Initialize git submodules."
-git submodule init
-git submodule update
+echo "Initialize git repos."
+git clone git@github.com:ohmyzsh/ohmyzsh.git oh-my-zsh
+git clone git@github.com:romkatv/powerlevel10k.git zsh_custom/themes/powerlevel10k
 
 echo "Deploy zsh config."
-rm ~/.zshrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/p10k.zsh ~/.p10k.zsh
 
 echo "Deploy vim config."
-rm -r ~/.vim
 ln -s ~/dotfiles/vim ~/.vim
-rm ~/.vimrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
 
 echo "Deploy tmux config."
-rm ~/.tmux.conf
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
 # echo "Deploy timewarrior config."
